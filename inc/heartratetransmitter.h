e0f12539b9f8e013bdbe078029ee3acc89192b36
lavender <fustyparent41@protonmail.com>
2020-10-25 00:23:02 +0900

    Initial commit

---
+++ inc/heartratetransmitter.h
@@ -0,0 +1,19 @@
+#ifndef __heartratetransmitter_H__
+#define __heartratetransmitter_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "heartratetransmitter"
+
+#if !defined(PACKAGE)
+#define PACKAGE "my.watch.heartratetransmitter"
+#endif
+
+#endif /* __heartratetransmitter_H__ */
